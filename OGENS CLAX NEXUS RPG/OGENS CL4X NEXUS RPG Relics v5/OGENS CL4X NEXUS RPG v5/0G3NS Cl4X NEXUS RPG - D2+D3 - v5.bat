c15c3c7d20450fed92eefb54e3c79db50ff1e6b5
x0G3NDrysOGx <ogkushdro360@gmail.com>
2025-06-12 16:18:11 -0500

    Update 0G3NS Cl4X NEXUS RPG - D2+D3 - v5.bat

--- OGENS CLAX NEXUS RPG/OGENS CL4X NEXUS RPG Relics v5/OGENS CL4X NEXUS RPG v5/0G3NS Cl4X NEXUS RPG - D2+D3 - v5.bat
+++ OGENS CLAX NEXUS RPG/OGENS CL4X NEXUS RPG Relics v5/OGENS CL4X NEXUS RPG v5/0G3NS Cl4X NEXUS RPG - D2+D3 - v5.bat
@@ -36,7 +36,7 @@ set /a xp=0
 set /a xp_needed=50
 
 rem Define companions
-set "companions=DrysOG Baked Brandon Spvcestep LeafChicken Slink Toady Crabman DarkSkitZo toqer KenshinGM CEnnis91 XoraLoyal alfalfa1 Paramount JohnnyTest CuddlyWolf Jgunishka Moosehead Shinfuji Agent21iXi Firo Suprafast Bampy RonyRollin"
+set "companions=DrysOG Baked Akihimura Brandon CatzBrownout Spvcestep LeafChicken Slink Toady Crabman DarkSkitZo toqer KenshinGM CEnnis91 XoraLoyal alfalfa1 Paramount JohnnyTest CuddlyWolf Jgunishka Moosehead Shinfuji Agent21iXi Firo Suprafast BadassBampy"
 
 rem Initialize companion stats
 for %%a in (!companions!) do (
